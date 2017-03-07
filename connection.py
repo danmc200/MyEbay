a1b56ad7c2f6ecadc85a4c6567f66abf0a33ab44
danmc200 <danielboydelatour@gmail.com>
2017-03-07 08:17:55 -0500

    update connection.py with sort options and item filter arrays

--- connection.py
+++ connection.py
@@ -3,15 +3,17 @@ class Connection ():
 	def __init__(self, app_id):
 		self.app_id = app_id
 		self.url = ""
+		self.sort_orders=["BidCountFewest", "BidCountMost", "CountryAscending", "CountryDescending", "CurrentPriceHighest", "DistanceNearest", "EndTimeSoonest", "PricePlusShippingHighest", "PricePlusShippingLowest", "StartTimeNewest"]
 
+		self.item_filters=[ "AvailableTo", "BestOfferOnly", "CharityOnly", "Condition", "Currency", "EndTimeFrom", "EndTimeTo", "ExcludeAutoPay", "ExcludeCategory", "ExcludeSeller", "ExpeditedShippingType", "FeaturedOnly", "FeedbackScoreMax", "FeedbackScoreMin", "FreeShippingOnly", "GetItFastOnly", "HideDuplicateItems", "ListedIn", "ListingType", "LocalPickupOnly", "LocalSearchOnly", "LocatedIn", "LotsOnly", "MaxBids", "MaxDistance", "MaxHandlingTime", "MaxPrice", "MaxQuantity", "MinBids", "MinPrice", "MinQuantity", "ModTimeFrom", "OutletSellerOnly", "PaymentMethod", "ReturnsAcceptedOnly", "Seller", "SellerBusinessType", "StartTimeFrom", "ValueBoxInventory", "WorldOfGoodOnly"]
 	def build(self):
 		base_url="http://svcs.ebay.com/services/search/FindingService/v1?"
 		operation_name="OPERATION-NAME=findItemsByKeywords"
 		security_appname="&SECURITY-APPNAME=" + self.app_id 
 		response_format="&RESPONSE-DATA-FORMAT=XML"
 		keywords="&keywords=nintendo%20ds"
-		item_filter_name_value="&itemFilter%280%29.name=Condition&itemFilter%280%29.value=New"
-		item_filter_name_value2="&itemFilter%281%29.name=ListingType&itemFilter%281%29.value=FixedPrice"
+		item_filter_name_value="&itemFilter.name=Condition&itemFilter.value=New"
+		item_filter_name_value2="&itemFilter.name=ListingType&itemFilter.value=FixedPrice"
 		sort_order="&sortOrder=PricePlusShippingLowest"
 		self.url=base_url + operation_name + security_appname + response_format + keywords + item_filter_name_value + item_filter_name_value2 + sort_order
 
